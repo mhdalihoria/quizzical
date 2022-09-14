199ea5679d8dd00a6971b2a5d76082b1fe1053e0
Mhd Ali Houria <alihoria123456@gmail.com>
2022-09-14 20:24:37 +0300

    updating "selected answer" logic

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -9,21 +9,22 @@ function Question({id, question, correctAnswer, answers, isFinished }) {
     setSelectedAnswer(answer)
   }
 
+  
   const answerBtns = answers.split(",").map((answer, index) => {
     // if the game is finished, and I have an answer selected, and I have an answer, that matches the value of the correct answer give me access to: 
-      if (isFinished && isChecked[index] && answer === correctAnswer) {
+      // if (isFinished && isChecked[index] && answer === correctAnswer) {
         // updateScore(1)
-      }
-      console.log(score);
+      // }
+      // console.log(score);
     
     const answerStyle =
       isFinished && answer === correctAnswer
         ? "answer-correct"
-        : isFinished && isChecked[index] && answer !== correctAnswer
+        : isFinished && selectedAnswer === answer && answer !== correctAnswer
         ? "answer-incorrect"
-        : isFinished && !isChecked[index] && answer !== correctAnswer
+        : isFinished && selectedAnswer !== answer !== correctAnswer
         ? "answer-unselected"
-        : isChecked[index] && "answer-selected";
+        : selectedAnswer === answer && "answer-selected";
 
     return (
       <button
