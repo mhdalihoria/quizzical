af3cfb9cbeac883681caafe847511e97dc2541c3
Mhd Ali Houria <alihoria123456@gmail.com>
2022-09-24 18:36:16 +0300

    Minor UI Improvement

--- src/pages/Intro.jsx
+++ src/pages/Intro.jsx
@@ -8,7 +8,7 @@ function Intro() {
       <h1 className="intro-title"> Quizzical </h1>
       <h6 className="intro-description">Challenge your knowledge</h6>
       <div className="intro-btn-container">
-        <Link to="/settings"><button className="intro-btn settings">Settings</button></Link>
+        <Link to="/settings"><button className="intro-btn settings">Custom Quiz</button></Link>
         <Link to="/quiz"><button className="intro-btn quiz">Quiz</button></Link>
       </div>
     </div>
